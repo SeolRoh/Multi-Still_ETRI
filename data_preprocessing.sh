31af8c8f067cb10c1f08f01a98de8c61c5d98cda
jo1132 <67040842+jo1132@users.noreply.github.com>
2023-05-01 02:03:46 +0900

    Update data_preprocessing.sh

--- data_preprocessing.sh
+++ data_preprocessing.sh
@@ -1,2 +1,2 @@
-python KEMDy_preprocessing.py
-python Data_Balancing.py
+python KEMDy_preprocessing.py # KEMDy19, KEMD20 데이터셋 읽고, 저장
+python Data_Balancing.py # 음성데이터가 존재하지 않는 데이터 정리 및 train, test 분리

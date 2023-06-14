541733a16a62cab3cad7e9bca1f1ec7a81b4d8c5
hyeon-ki jo <jhk1132@gmail.com>
2023-06-14 16:53:52 +0000

    add valid dataset

--- data_preprocessing.sh
+++ data_preprocessing.sh
@@ -1,2 +1,5 @@
+echo "start KEMDy data preprocessing..."
 python KEMDy_preprocessing.py # KEMDy19, KEMD20 데이터셋 읽고, 저장
+
+echo "start Data Balancing"
 python Data_Balancing.py # 음성데이터가 존재하지 않는 데이터 정리 및 train, test 분리
